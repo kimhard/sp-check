1bbf06c12d15b970bd752f29d65939b20978f398
ASUS <ASUS@DESKTOP-J5UJA7S>
2023-07-20 02:21:06 +0900

    학습 종료

---
+++ SpringMVC06/src/main/java/kr/board/entity/board.sql
@@ -0,0 +1,37 @@
+create table myboard(
+	idx int not null auto_increment,
+	memID varchar(20) not null;
+	title varchar(100) not null,
+	content varchar(2000) not null,
+	writer varchar(30) not null,
+	indate datetime default now(),
+	count int default 0,
+	primary key(idx)
+);
+
+create table mem_stbl(
+	memIdx int not null, -- 자동증가X
+	memID varchar(20) not null, 
+	memPassword varchar(68) not null,
+	memName varchar(20) not null,
+	memAge int,
+	memGender varchar(20),
+	memEmail varchar(50),
+	memProfile varchar(50),
+	primary key(memID)
+);
+
+create table mem_auth(
+	no int not null auto_increment,
+	memID varchar(50) not null,
+	auth varchar(50) not null,
+	primary key(no),
+	constraint fk_member_auth foreign key(memID)
+	references mem_stbl(memID)
+);
+
+select * from mem_stbl;
+delete from mem_stbl;
+delete from mem_auth;
+delete from myboard;
+SET FOREIGN_KEY_CHECKS = 0;
---
+++ SpringMVC06/src/main/java/kr/board/security/MemberUserDetailsService.java
@@ -0,0 +1,27 @@
+package kr.board.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import kr.board.entity.Member;
+import kr.board.entity.MemberUser;
+import kr.board.mapper.MemberMapper;
+
+public class MemberUserDetailsService implements UserDetailsService{
+	@Autowired
+	private MemberMapper memberMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		Member mvo = memberMapper.memLogin(username);
+		//UserDetails implements --->User -> extends ---> MemberUser
+		if(mvo != null) {
+			return new MemberUser(mvo); // new MemberUser(mvo); //member정보와 auth 정보를 memberuser에넣어주기위해
+	}else {
+		throw new UsernameNotFoundException("user with username" + username + "does not exist.");
+	}
+
+}
+}
